a03866999d2a00d65c1cf994d0f2dd6978e001f8
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-12-01 14:56:37 -0700

    private nav - add name to top

--- client/src/components/Nav/PrivateNav.js
+++ client/src/components/Nav/PrivateNav.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function PrivateNav({setAuth}) {
+
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: {token: localStorage.token}
+      });
+
+
+      const parseRes = await response.json();
+
+      setName(parseRes.first_name)
+
+
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  const logout = e => {
+    e.preventDefault();
+
+     localStorage.removeItem("token");
+
+     setAuth(false);
+  }
+  
+  useEffect(() => {
+    getName()
+  }, [])
+
+
   return (
-    <div>
+    
+    <div className="nav">
       <h1>PRIVATE NAV</h1>
-      <button onClick={() => setAuth(false)}>Logout</button>
+      <div className="nav--left-side">
+
+      </div>
+      <div className="nav--middle">
+
+      </div>
+      <div className="nav--right-side">
+        <a>Welcome, {name}!</a>
+        <button onClick={(e) => logout(e)}>Logout</button>
+      </div>
     </div>
   )
 }
